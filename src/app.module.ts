47edacac31a57826ff44224510435391fad7e39b
Pavlenko-Artem <artempavlenko1703@gmail.com>
2026-02-28 01:53:33 +0300

    начало работы с .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './modules/test/test.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: [TestModule],
+	imports: [TestModule, ConfigModule.forRoot()],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+// import { configuration } from './config/configuration';
+
+interface EnvironmentVariables {
+	PORT: number;
+	// TIMEOUT: {
+	// 	TEST: number;
+	// };
+	TIMEOUT: string;
+}
+
+// enum ConfigEnum {
+// 	PORT = 'PORT',
+// 	TIMEOUT = 'TIMEOUT',
+// }
+
+// const TestObject = {
+// 	PORT: 'PORT',
+// 	TIMEOUT: 'TIMEOUT',
+// } as const;
 
 @Injectable()
 export class AppService {
+	// constructor(private configService: ConfigService<{ database: { host: string } }>) {}
+	constructor(private configService: ConfigService<EnvironmentVariables>) {}
+
 	getHello(): string {
+		// const dbUser = this.configService.get<number>('PORT');
+		// const test = process.env.PORT;
+		// const test = this.configService.get<EnvironmentVariables['PORT']>(TestObject.PORT);
+		// const test = this.configService.get(TestObject.TIMEOUT, { infer: true });
+		// console.log(dbUser);
+		// console.log(test);
+
 		return 'Hello World!';
 	}
 }
