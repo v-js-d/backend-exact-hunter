68a785092660cd4222d1aea3226b1861d06386b1
Pavlenko-Artem <artempavlenko1703@gmail.com>
2026-03-02 23:18:18 +0300

    добавлен tsc-files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
 export default {
-	'*.{ts,tsx}': ['eslint --fix', 'prettier --write', () => 'tsc -p tsconfig.json --noEmit --pretty'],
+	'*.{ts}': ['eslint --fix', 'prettier --write', 'tsc-files --noEmit --pretty'],
 	'*': 'prettier --write --ignore-unknown',
 };
 // 'tsc-files -p tsconfig.json --noEmit --pretty',
