72b5ae5b069ddb3554be8b8487684106827557b2
Pavlenko-Artem <artempavlenko1703@gmail.com>
2026-04-14 21:08:23 +0300

    add table for auth

---
+++ prisma/migrations/20260414184956_auth_compoanies_profiles_models/migration.sql
@@ -0,0 +1,213 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "auth";
+
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "companies";
+
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "profiles";
+
+-- CreateEnum
+CREATE TYPE "auth"."user_role" AS ENUM ('CANDIDATE', 'EMPLOYER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "profiles"."candidate_profiles" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "first_name" VARCHAR(100),
+    "last_name" VARCHAR(100),
+    "middle_name" VARCHAR(100),
+    "birthday" TIMESTAMPTZ,
+    "phone" VARCHAR(20),
+    "contact_email" VARCHAR(255),
+    "contact_phone" VARCHAR(20),
+    "avatar" VARCHAR(500),
+    "bio" TEXT,
+    "location" VARCHAR(255),
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "candidate_profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "companies"."companies" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "inn" VARCHAR(20),
+    "company_type_id" UUID NOT NULL,
+    "owner_id" UUID NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+    "created_by" UUID,
+    "updated_by" UUID,
+
+    CONSTRAINT "companies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "companies"."company_types" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "slug" VARCHAR(100) NOT NULL,
+    "description" TEXT,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "company_types_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "auth"."hr_roles" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "slug" VARCHAR(50) NOT NULL,
+    "description" TEXT,
+    "permissions" JSONB,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "hr_roles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "auth"."role_contexts" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "user_role" "auth"."user_role" NOT NULL,
+    "company_id" UUID,
+    "hr_role_id" UUID,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "role_contexts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "auth"."tokens" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "role_context_id" UUID NOT NULL,
+    "refresh_token" VARCHAR(500) NOT NULL,
+    "device_id" VARCHAR(255) NOT NULL,
+    "device_name" VARCHAR(255),
+    "user_agent" TEXT,
+    "ip_address" VARCHAR(45),
+    "expires_at" TIMESTAMP(6) NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) NOT NULL,
+
+    CONSTRAINT "tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "auth"."users" (
+    "id" UUID NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "is_activated" BOOLEAN NOT NULL DEFAULT false,
+    "activation_link" VARCHAR(255),
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) NOT NULL,
+    "created_by" UUID,
+    "updated_by" UUID,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "candidate_profiles_user_id_key" ON "profiles"."candidate_profiles"("user_id");
+
+-- CreateIndex
+CREATE INDEX "candidate_profiles_user_id_idx" ON "profiles"."candidate_profiles"("user_id");
+
+-- CreateIndex
+CREATE INDEX "companies_owner_id_inn_company_type_id_created_by_updated_b_idx" ON "companies"."companies"("owner_id", "inn", "company_type_id", "created_by", "updated_by");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "company_types_name_key" ON "companies"."company_types"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "company_types_slug_key" ON "companies"."company_types"("slug");
+
+-- CreateIndex
+CREATE INDEX "company_types_name_slug_idx" ON "companies"."company_types"("name", "slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "hr_roles_name_key" ON "auth"."hr_roles"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "hr_roles_slug_key" ON "auth"."hr_roles"("slug");
+
+-- CreateIndex
+CREATE INDEX "hr_roles_name_slug_is_active_idx" ON "auth"."hr_roles"("name", "slug", "is_active");
+
+-- CreateIndex
+CREATE INDEX "role_contexts_user_id_user_role_company_id_hr_role_id_idx" ON "auth"."role_contexts"("user_id", "user_role", "company_id", "hr_role_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tokens_refresh_token_key" ON "auth"."tokens"("refresh_token");
+
+-- CreateIndex
+CREATE INDEX "tokens_user_id_idx" ON "auth"."tokens"("user_id");
+
+-- CreateIndex
+CREATE INDEX "tokens_role_context_id_idx" ON "auth"."tokens"("role_context_id");
+
+-- CreateIndex
+CREATE INDEX "tokens_refresh_token_idx" ON "auth"."tokens"("refresh_token");
+
+-- CreateIndex
+CREATE INDEX "tokens_device_id_idx" ON "auth"."tokens"("device_id");
+
+-- CreateIndex
+CREATE INDEX "tokens_expires_at_idx" ON "auth"."tokens"("expires_at");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tokens_user_id_role_context_id_device_id_key" ON "auth"."tokens"("user_id", "role_context_id", "device_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "auth"."users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_activation_link_key" ON "auth"."users"("activation_link");
+
+-- CreateIndex
+CREATE INDEX "users_email_created_by_updated_by_idx" ON "auth"."users"("email", "created_by", "updated_by");
+
+-- AddForeignKey
+ALTER TABLE "profiles"."candidate_profiles" ADD CONSTRAINT "candidate_profiles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "companies"."companies" ADD CONSTRAINT "companies_company_type_id_fkey" FOREIGN KEY ("company_type_id") REFERENCES "companies"."company_types"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "companies"."companies" ADD CONSTRAINT "companies_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "auth"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "companies"."companies" ADD CONSTRAINT "companies_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "auth"."users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "companies"."companies" ADD CONSTRAINT "companies_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "auth"."users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth"."role_contexts" ADD CONSTRAINT "role_contexts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth"."role_contexts" ADD CONSTRAINT "role_contexts_company_id_fkey" FOREIGN KEY ("company_id") REFERENCES "companies"."companies"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth"."role_contexts" ADD CONSTRAINT "role_contexts_hr_role_id_fkey" FOREIGN KEY ("hr_role_id") REFERENCES "auth"."hr_roles"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth"."tokens" ADD CONSTRAINT "tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth"."tokens" ADD CONSTRAINT "tokens_role_context_id_fkey" FOREIGN KEY ("role_context_id") REFERENCES "auth"."role_contexts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth"."users" ADD CONSTRAINT "users_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "auth"."users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth"."users" ADD CONSTRAINT "users_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "auth"."users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
